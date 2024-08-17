fd27473e95fb663660e337807f3e4edacf96f554
ColinYYCC <121481091+ColinYYCC@users.noreply.github.com>
2024-08-18 05:24:18 +0800

    Update MyMihomoScript.js

--- MyClashScript.js
+++ MyClashScript.js
@@ -1,8 +1,8 @@
-// 参考 Lucy（@Repcz） 示例 Script 配置
+// 参考 @Repzc(Luzy) 示例 Script 配置
 //
-// Clash Verg Rev Version ≥ 1.7.2
+// Clash Verge Rev (Version ≥ 17.2) & Mihomo-Party (Version ≥ 0.5.8)
 //
-// 最后更新时间: 2024-08-15 21:30
+// 最后更新时间: 2024-08-17 20:10
 
 // 规则集通用配置
 const ruleProviderCommon = {
@@ -32,7 +32,6 @@ function main(config) {
   config["tcp-concurrent"] = true;
   config["allow-lan"] = true;
   config["ipv6"] = false;
-  config["mode"] = "rule";
   config["log-level"] = "info";
   config["find-process-mode"] = "strict";
   config["global-client-fingerprint"] = "chrome";
@@ -47,85 +46,13 @@ function main(config) {
     "ipv6": false,
     "enhanced-mode": "fake-ip",
     "fake-ip-range": "198.18.0.1/16",
-    "fake-ip-filter":[
-      "*.lan",
-      "*.direct",
-      "cable.auth.com",
-      "*.msftconnecttest.com",
-      "*.msftncsi.com",
-      "network-test.debian.org",
-      "detectportal.firefox.com",
-      "resolver1.opendns.com",
-      "*.srv.nintendo.net",
-      "*.stun.playstation.net",
-      "xbox.*.microsoft.com",
-      "*.xboxlive.com",
-      "stun.*",
-      "global.turn.twilio.com",
-      "global.stun.twilio.com",
-      "app.yinxiang.com",
-      "injections.adguard.org",
-      "local.adguard.org",
-      "cable.auth.com",
-      "localhost.*.qq.com",
-      "localhost.*.weixin.qq.com",
-      "*.logon.battlenet.com.cn",
-      "*.logon.battle.net",
-      "*.blzstatic.cn",
-      "music.163.com",
-      "*.music.163.com",
-      "*.126.net",
-      "musicapi.taihe.com",
-      "music.taihe.com",
-      "songsearch.kugou.com",
-      "trackercdn.kugou.com",
-      "*.kuwo.cn",
-      "api-jooxtt.sanook.com",
-      "api.joox.com",
-      "joox.com",
-      "y.qq.com",
-      "*.y.qq.com",
-      "streamoc.music.tc.qq.com",
-      "mobileoc.music.tc.qq.com",
-      "isure.stream.qqmusic.qq.com",
-      "dl.stream.qqmusic.qq.com",
-      "aqqmusic.tc.qq.com",
-      "amobile.music.tc.qq.com",
-      "*.xiami.com",
-      "*.music.migu.cn",
-      "music.migu.cn",
-      "proxy.golang.org",
-      "*.mcdn.bilivideo.cn",
-      "*.cmpassport.com",
-      "id6.me",
-      "open.e.189.cn",
-      "mdn.open.wo.cn",
-      "opencloud.wostore.cn",
-      "auth.wosms.cn",
-      "*.jegotrip.com.cn",
-      "*.icitymobile.mobi",
-      "*.pingan.com.cn",
-      "*.cmbchina.com",
-      "*.10099.com.cn",
-      "*.microdone.cn",
-      "pool.ntp.org",
-      "*.pool.ntp.org",
-      "ntp.*.com",
-      "time.*.com",
-      "ntp?.*.com",
-      "time?.*.com",
-      "time.*.gov",
-      "time.*.edu.cn",
-      "*.ntp.org.cn",
-      "PDC._msDCS.*.*",
-      "DC._msDCS.*.*",
-      "GC._msDCS.*.*"
-    ],
-    "default-nameserver": ["9.9.9.9", "149.112.112.112"],
-    "nameserver": ["9.9.9.9", "149.112.112.112"],
+    "fake-ip-filter":["+.lan", "*", "+.local"],
+    "default-nameserver": ["223.5.5.5", "119.29.29.29", "system"],
+    "nameserver": ["223.5.5.5", "119.29.29.29"],
     "nameserver-policy":{
       "geosite:cn": "system",
-      "geosite:gfw,geolocation-!cn": ["quic://223.5.5.5", "quic://223.6.6.6", "https://1.12.12.12/dns-query", "https://120.53.53.53/dns-query"]
+      "geosite:gfw”:”223.5.5.5", "119.29.29.29",
+      "geolocation-!cn": ["quic://223.5.5.5", "quic://223.6.6.6", "https://1.12.12.12/dns-query", "https://120.53.53.53/dns-query"]
     }
   };
 
@@ -165,7 +92,7 @@ function main(config) {
 
   // 覆盖策略组
   config["proxy-groups"] = [
-    {
+     {
       ...groupBaseOption,
       "name": "手动切换",
       "type": "select",
@@ -339,18 +266,11 @@ function main(config) {
       "include-all": true,
       "filter": "^(?=.*(.))(?!.*((?i)群|邀请|返利|循环|官网|客服|网站|网址|获取|订阅|流量|到期|机场|下次|版本|官址|备用|过期|已用|联系|邮箱|工单|贩卖|通知|倒卖|防止|国内|地址|频道|无法|说明|使用|提示|特别|访问|支持|教程|关注|更新|作者|加入|(\b(USE|USED|TOTAL|EXPIRE|EMAIL|Panel|Channel|Author)\b|(\d{4}-\d{2}-\d{2}|\d+G)))).*$'",
       "icon": "https://raw.githubusercontent.com/Koolson/Qure/master/IconSet/Color/Available.png"
-    },
-
+    }
   ];
 
   // 覆盖规则集
   config["rule-providers"] = {
-    "Lan": {
-      ...ruleProviderCommon,
-      "behavior": "classical",
-      "url": "https://github.com/Repcz/Tool/raw/X/Clash/Rules/Lan.list",
-      "path": "./rule-providers/Lan.list"
-    },
       "Sogouinput": {
       ...ruleProviderCommon,
       "behavior": "classical",
@@ -387,18 +307,6 @@ function main(config) {
       "url": "https://ruleset.skk.moe/List/non_ip/apple_services.conf",
       "path": "./rule-providers/apple_services.conf"
     },
-    "常见静态 CDN": {
-      ...ruleProviderCommon,
-      "behavior": "domain",
-      "url": "https://ruleset.skk.moe/Clash/domainset/cdn.txt",
-      "path": "./rule-providers/cdn_domainset.txt"
-    },
-    "常见静态 CDN": {
-      ...ruleProviderCommon,
-      "behavior": "domain",
-      "url": "https://ruleset.skk.moe/Clash/non_ip/cdn.txt",
-      "path": "./rule-providers/cdn_non_ip.txt"
-    },
     "Google": {
       ...ruleProviderCommon,
       "behavior": "classical",
@@ -414,7 +322,7 @@ function main(config) {
     "Telegram": {
       ...ruleProviderCommon,
       "behavior": "classical",
-      "url": "https://github.com/LucaLin233/Luca_Conf/blob/main/Surge/Rule/Telegram.list?raw=true",
+      "url": "https://github.com/Repcz/Tool/raw/X/Clash/Rules/Telegram.list",
       "path": "./rule-providers/Telegram.list"
     },
     "Twitter": {
@@ -492,7 +400,7 @@ function main(config) {
     "Github": {
       ...ruleProviderCommon,
       "behavior": "classical",
-      "url": "https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Surge/GitHub/GitHub.list",
+      "url": "https://github.com/Repcz/Tool/raw/X/Clash/Rules/Github.list",
       "path": "./rule-providers/Github.list"
     },
     "Microsoft": {
@@ -501,22 +409,16 @@ function main(config) {
       "url": "https://github.com/Repcz/Tool/raw/X/Clash/Rules/Microsoft.list",
       "path": "./rule-providers/Microsoft.list"
     },
-    "Microsoft国内CND": {
+    "Lan": {
       ...ruleProviderCommon,
       "behavior": "classical",
-      "url": "https://ruleset.skk.moe/List/non_ip/microsoft_cdn.conf",
-      "path": "./rule-providers/microsoft_cdn.conf"
-    },
-    "ProxyMedia": {
-      ...ruleProviderCommon,
-      "behavior": "classical",
-      "url": "https://github.com/Repcz/Tool/raw/X/Clash/Rules/ProxyMedia.list",
-      "path": "./rule-providers/ProxyMedia.list"
+      "url": "https://github.com/Repcz/Tool/raw/X/Clash/Rules/Lan.list",
+      "path": "./rule-providers/Lan.list"
     },
     "ProxyGFW": {
       ...ruleProviderCommon,
       "behavior": "classical",
-      "url": "https://ruleset.skk.moe/Clash/non_ip/global.txt",
+      "url": "https://github.com/Repcz/Tool/raw/X/Clash/Rules/ProxyGFW.list",
       "path": "./rule-providers/ProxyGFW.list"
     },
     "ChinaDomain": {
@@ -559,7 +461,6 @@ function main(config) {
     "RULE-SET,adrules,广告拦截",
     "RULE-SET,Reject_non_ip,广告拦截",
     "DOMAIN-SUFFIX,zoom.us,国外网站",
-    "RULE-SET,常见静态 CDN,国外网站",
     "RULE-SET,AI,AI",
     "RULE-SET,Apple Service,苹果服务",
     "RULE-SET,YouTube,谷歌服务",
@@ -586,7 +487,7 @@ function main(config) {
     "RULE-SET,China_ip,DIRECT",
     "GEOIP,lan,DIRECT",
     "GEOIP,CN,DIRECT",
-    "MATCH,漏网之鱼"
+    "MATCH,兜底分流"
   ];
 
   // 返回修改后的配置
