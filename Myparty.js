41e8302e9f079d1fec3949aba186984dc09bbd83
ColinYYCC <121481091+ColinYYCC@users.noreply.github.com>
2024-08-20 17:54:55 +0800

    Update Myparty.js

--- Myparty.js
+++ Myparty.js
@@ -664,33 +664,6 @@ function main(config) {
     "MATCH,漏网之鱼"
   ];
 
-  function main(config) {
-  const removed = []; // 过滤后不含任何节点将被移除的group
-  config["proxy-groups"] = (config["proxy-groups"] ?? [])
-    .map(group => {
-      if (multipliers[group?.name]) {
-        group["proxies"] = (group["proxies"] ?? []).filter(proxy => {
-          const multiplier = regex.exec(proxy)?.[1];
-          return multipliersOnly
-            ? multiplier !== undefined && Number(multiplier) <= multipliers[group.name]
-            : multiplier === undefined || Number(multiplier) <= multipliers[group.name];
-        });
-        if (group["proxies"]?.length > 0) {
-          return group;
-        }
-        removed.push(group.name);
-      } else {
-        return group;
-      }
-    })
-    .filter(Boolean);
-  if (removed.length > 0) {
-    // 移除代理组的引用
-    config["proxy-groups"].forEach(group => {group["proxies"] = group["proxies"].filter(proxy => !removed.includes(proxy));});
-    // 移除规则的引用
-    config["rules"] = (config["rules"] ?? []).filter(rule => rule && removed.every(name => !rule.endsWith(name) && !rule.endsWith(`${name},no-resolve`)));
-  }
-
   // 返回修改后的配置
   return config;
 }
