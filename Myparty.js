dd916d206a165027b13227e18cd8ea158a782aba
ColinYYCC <121481091+ColinYYCC@users.noreply.github.com>
2024-09-20 19:05:23 +0800

    Update Myparty.js

--- Myparty.js
+++ Myparty.js
@@ -318,7 +318,7 @@ function main(config) {
       "path": "./rules/reject.txt",
       ...ruleProviderCommon,
     },
-     "Reject_Extra": {
+    "Reject_Extra": {
       "behavior": "domain",
       "url": "https://ruleset.skk.moe/Clash/domainset/reject_extra.txt",
       "path": "./rules/reject_extra.txt",
